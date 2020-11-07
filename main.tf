19df250d3476579384d33dcf4f38b2e1c7765ed9
earroyoh <enrique.arroyo@gmail.com>
2020-11-07 12:14:04 +0100

    modified:   main.tf 	new file:   manage.py

--- main.tf
+++ main.tf
@@ -11,24 +11,28 @@ resource "docker_container" "dadbot" {
   hostname  = "dadbot"
   ports {
     internal = 5000
-    external = 5000
+  }
+  ports {
+    internal = 5002
   }
   ports {
     internal = 5005
-    external = 5005
   }
   ports {
     internal = 5055
     external = 5055
   }
+  ports {
+    internal = 8000
+  }
   volumes {
     host_path = "/home/debian/workspace/Dadbot/models"
-    container_path = "/app/Dadbot/models"
+    container_path = "/app/models"
     volume_name = "models"
   }
   volumes {
     host_path = "/home/debian/workspace/Dadbot/data"
-    container_path = "/app/Dadbot/data"
+    container_path = "/app/data"
     volume_name = "data"
   }
   working_dir = "/app"
