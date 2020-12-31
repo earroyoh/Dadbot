63df0f081c7cb10cb48f93414a4832c6fd139b52
earroyoh <enrique.arroyo@gmail.com>
2020-12-31 10:15:34 +0100

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,43 @@ resource "docker_image" "dadbot" {
   keep_locally = true
 }
 
-resource "docker_container" "dadbot-trainer" {
+resource "docker_image" "dadbot-api" {
+  name         = "dadbot-api:1.0"
+  keep_locally = true
+}
+
+resource "docker_network" "backend-net" {
+  name = "backend-net"
+  internal = true
+}
+
+resource "docker_network" "frontend-net" {
+  name = "frontend-net"
+}
+
+#resource "docker_volume" "nvidia_models" {
+#  name = "nvidia_models"
+#}
+
+resource "docker_container" "dadbot-actions" {
   image = docker_image.dadbot.name
+  name  = "dadbot-actions"
+  hostname  = "dadbot-actions"
+  ports {
+    internal = 5055
+    external = 5055
+  }
+  networks_advanced {
+    name = "backend-net"
+    aliases = ["private"]
+  }
+
+  working_dir = "/app"
+  user = 1000
+}
+
+resource "docker_container" "dadbot-trainer" {
+  image = docker_image.dadbot-api.name
   name  = "dadbot-trainer"
   hostname  = "dadbot-trainer"
   ports {
@@ -19,36 +54,23 @@ resource "docker_container" "dadbot-trainer" {
     internal = 5005
   }
   volumes {
-    host_path = "/home/debian/workspace/Dadbot/models"
-    container_path = "/app/models"
+    host_path = "/home/debian/workspace/Dadbot"
+    container_path = "/app/Dadbot"
     volume_name = "models"
   }
-  volumes {
-    host_path = "/home/debian/workspace/Dadbot/data"
-    container_path = "/app/data"
-    volume_name = "data"
+  networks_advanced {
+    name = "backend-net"
+    aliases = ["private"]
   }
-  working_dir = "/app"
+  working_dir = "/app/Dadbot"
   user = 1000
-  command = ["run", "python3", "-m", "rasa", "train", "--debug"]
+  command = ["python3", "-m", "rasa", "train", "--debug"]
 }
 
-resource "docker_container" "dadbot-actions" {
-  image = docker_image.dadbot.name
-  name  = "dadbot-actions"
-  hostname  = "dadbot-actions"
-  ports {
-    internal = 5055
-    external = 5055
-  }
-  working_dir = "/app"
-  user = 1000
-}
-
-resource "docker_container" "dadbot-api" {
-  image = docker_image.dadbot.name
-  name  = "dadbot-api"
-  hostname  = "dadbot-api"
+resource "docker_container" "dadbot-connector" {
+  image = docker_image.dadbot-api.name
+  name  = "dadbot-connector"
+  hostname  = "dadbot-connector"
   ports {
     internal = 5005
     external = 5005
@@ -59,18 +81,18 @@ resource "docker_container" "dadbot-api" {
     volume_name = "nvidia_models"
   }
   volumes {
-    host_path = "/home/debian/workspace/Dadbot/models"
-    container_path = "/app/models"
-    volume_name = "models"
+    host_path = "/home/debian/workspace/Dadbot"
+    container_path = "/app/Dadbot"
+    volume_name = "rasa-app"
   }
-  volumes {
-    host_path = "/home/debian/workspace/Dadbot/data"
-    container_path = "/app/data"
-    volume_name = "data"
+  networks_advanced {
+    name = "backend-net"
+    aliases = ["private"]
   }
-  working_dir = "/app"
+
+  working_dir = "/app/Dadbot"
   user = 1000
-  command = ["run", "python3", "-m", "rasa", "run", "-m", "/app/models", "--enable-api", "--cors", "'*'", "--debug"]
+  command = ["python3", "-m", "rasa", "run", "--enable-api", "--cors", "'*'", "--connector", "voice_connector.ChatInput", "--debug"]
 
   depends_on = [docker_container.dadbot-trainer]
 }
@@ -83,6 +105,15 @@ resource "docker_container" "dadbot-web" {
     internal = 8000
     external = 8000
   }
+  networks_advanced {
+    name = "frontend-net"
+    aliases = ["public"]
+  }
+  networks_advanced {
+    name = "backend-net"
+    aliases = ["private"]
+  }
+
   working_dir = "/app"
   user = 1001
   command = ["run", "python3", "dadbot.py"]
