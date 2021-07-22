6c9e8128c8da2582136eeafe9fc21dfeceda8f03
earroyoh <enrique.arroyo@gmail.com>
2021-07-22 22:42:38 +0200

    modified:   actions/actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -142,7 +142,7 @@ class OpenAI_chat(Action):
 
         openai.api_key = os.getenv("OPENAI_API_KEY")
         openai_response = openai.Completion.create(engine="davinci", max_tokens=150, prompt=prompt, stop=stop, temperature=0.4, top_p=1, frequency_penalty=0.0, presence_penalty=0.6)
-        response = openai_response["choices"][0]["text"].replace("  ","\n").split("\n")[0]
+        response = openai_response["choices"][0]["text"].replace(" Humano:","\n").split("\n")[0]
 
         dispatcher.utter_message(text=format(response))
         return [SlotSet("GPT3", "true")]
